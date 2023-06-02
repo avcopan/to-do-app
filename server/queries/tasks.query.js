52f455c3a16f53d23102c36f8885388e0d34908d
Andreas Copan <avcopan@gmail.com>
2023-06-02 14:37:51 -0400

    Sets up INSERT and UPDATE queries

--- server/queries/tasks.query.js
+++ server/queries/tasks.query.js
@@ -11,4 +11,35 @@ const getTasks = () => {
     });
 };
 
+const addTask = (task) => {
+  const queryString = `
+    INSERT INTO tasks (content)
+    VALUES ($1);
+  `;
+  const queryParams = [task.content];
+
+  return pool
+    .query(queryString, queryParams)
+    .catch((error) => {
+      throw new Error(error);
+    });
+}
+
+const editTask = (id, task) => {
+  const queryString = `
+    UPDATE tasks
+    SET content = $2, completed = $3
+    WHERE id = $1;
+  `;
+  const queryParams = [id, task.content, task.completed];
+
+  return pool
+    .query(queryString, queryParams)
+    .catch((error) => {
+      throw new Error(error);
+    });
+}
+
+// addTask({content: 'Do laundry'});
+editTask(5, {content: 'Do laundry', completed: true});
 getTasks().then(console.log);
