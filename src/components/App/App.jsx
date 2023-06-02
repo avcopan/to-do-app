f91b2f192c7db88cfef0a9e17aec9fc4f8bab8c1
Andreas Copan <avcopan@gmail.com>
2023-06-02 16:01:13 -0400

    Posts current todo list to the DOM

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,36 @@
-import {useState} from 'react';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-function App () {
-  
+const getTodos = () => {
+  return axios
+    .get("/todo")
+    .then((res) => res.data)
+    .catch((error) => console.error(error));
+};
+
+function App() {
+  const [todoList, setTodoList] = useState([]);
+
+  useEffect(() => {
+    getTodos().then(setTodoList);
+  }, []);
+  console.log(todoList);
+
   return (
     <div>
       <h1>TO DO APP</h1>
+      <h2>To Do List:</h2>
+      <div>
+        {todoList.map((todo) => {
+          return (
+            <div key={todo.id}>
+                {todo.task} ({todo.completed ? 'Done' : 'Not done'})
+            </div>
+          )
+        })}
+      </div>
     </div>
   );
-
 }
 
-export default App
+export default App;
