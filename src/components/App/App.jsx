cc30bab1cf3a65e541ea8728a54197e52c8b9394
Andreas Copan <avcopan@gmail.com>
2023-06-05 17:29:15 -0400

    Separates axios requests into their own submodule

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,11 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
 import "./App.css";
+import {
+  getTodos,
+  addTodo,
+  editTodosCompleted,
+  removeTodos,
+} from "../../utils/axios";
 
 function App() {
   const [todos, setTodos] = useState([]);
@@ -11,55 +16,14 @@ function App() {
     getTodos().then(setTodos);
   }, []);
 
-  const getTodos = async () => {
-    try {
-      const res = await axios.get("/todo");
-      return res.data;
-    } catch (error) {
-      return console.error(error);
-    }
-  };
-
-  const addTodo = async (event) => {
-    event.preventDefault();
-    const newTodo = { task: taskInput };
-    setTaskInput("");
-
-    try {
-      await axios.post("/todo", newTodo);
-      await getTodos().then(setTodos);
-    } catch (error) {
-      return console.error(error);
-    }
-  };
-
-  const deleteTodos = async (todos) => {
-    try {
-      const idCsv = checkedIds.join(",");
-      console.log("End point", `/todo?ids=${idCsv}`);
-      await axios.delete(`/todo?ids=${idCsv}`);
-      await getTodos().then(setTodos);
-    } catch (error) {
-      throw new Error(error);
-    }
-  };
-
-  const updateTaskInput = (event) => {
-    setTaskInput(event.target.value);
-  };
-
   const updateChecked = (event, id) => {
-    if (event.target.checked && !checkedIds.includes(id)) {
+    if (event.target.checked) {
       checkedIds.push(id);
-    } else if (!event.target.checked) {
+    } else {
       checkedIds = checkedIds.filter((i) => i !== id);
     }
     console.log("Updating checkedIds:", checkedIds);
     setCheckedIds(checkedIds);
-  };
-
-  const markCheckedComplete = () => {
-    console.log("Marking checked!");
   };
 
   return (
@@ -82,16 +46,43 @@ function App() {
           );
         })}
       </div>
-      <button onClick={markCheckedComplete}>Mark Complete</button>
-      <button onClick={() => deleteTodos(checkedIds)}>Delete</button>
+      <button
+        onClick={() => {
+          editTodosCompleted(checkedIds, true).then(getTodos).then(setTodos);
+        }}
+      >
+        Complete
+      </button>
+      <button
+        onClick={() => {
+          editTodosCompleted(checkedIds, false).then(getTodos).then(setTodos);
+        }}
+      >
+        Incomplete
+      </button>
+      <button
+        onClick={() => {
+          removeTodos(checkedIds).then(getTodos).then(setTodos);
+        }}
+      >
+        Delete
+      </button>
       <h2>Add To Do Item:</h2>
-      <form onSubmit={addTodo}>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          addTodo(taskInput).then(getTodos).then(setTodos);
+          setTaskInput("");
+        }}
+      >
         <input
           type="text"
           id="taskInput"
           placeholder="Task"
           value={taskInput}
-          onChange={updateTaskInput}
+          onChange={(event) => {
+            setTaskInput(event.target.value);
+          }}
         />
         <button type="submit">Submit</button>
       </form>
---
+++ src/utils/axios.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+export const getTodos = async () => {
+  try {
+    const res = await axios.get("/todo");
+    const todos = res.data;
+    return todos.sort((a, b) => a.id - b.id);
+  } catch (error) {
+    return console.error(error);
+  }
+};
+
+export const addTodo = async (task) => {
+  const newTodo = { task: task };
+
+  try {
+    await axios.post("/todo", newTodo);
+  } catch (error) {
+    return console.error(error);
+  }
+};
+
+export const editTodosCompleted = async (ids, completed) => {
+  const idCsv = ids.join(",");
+
+  try {
+    await axios.put(`/todo?ids=${idCsv}&completed=${completed}`);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export const removeTodos = async (ids) => {
+  const idCsv = ids.join(",");
+
+  try {
+    await axios.delete(`/todo?ids=${idCsv}`);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
