eb09b89f882d5c5f3053c6c029e4edd01a232128
Andreas Copan <avcopan@gmail.com>
2023-06-05 14:31:41 -0400

    Implements endpoints for batch edit/remove requests

--- server/queries/todo.query.js
+++ server/queries/todo.query.js
@@ -1,29 +1,42 @@
 const pool = require("../modules/pool");
 
-const getTodos = () => {
+/** Get the todos currently in the database
+ */
+const getTodos = async () => {
   const queryString = "SELECT * FROM todos;";
 
-  return pool
-    .query(queryString)
-    .then((result) => result.rows)
-    .catch((error) => {
-      throw new Error(error);
-    });
+  try {
+    const result = await pool.query(queryString);
+    return result.rows;
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
-const addTodo = (todo) => {
+/** Add an individual todo to the database
+ *
+ * @param {Object} todo - The todo to be added
+ */
+const addTodo = async (todo) => {
   const queryString = `
     INSERT INTO todos (task)
     VALUES ($1);
   `;
   const queryParams = [todo.task];
 
-  return pool.query(queryString, queryParams).catch((error) => {
+  try {
+    return await pool.query(queryString, queryParams);
+  } catch (error) {
     throw new Error(error);
-  });
+  }
 };
 
-const editTodo = (id, todo) => {
+/** Edit an individual todo in the database
+ *
+ * @param {Number} id - The ID of the todo to be edited
+ * @param {Object} todo - The updated todo object
+ */
+const editTodo = async (id, todo) => {
   const queryString = `
     UPDATE todos
     SET task = $2, completed = $3
@@ -31,18 +44,72 @@ const editTodo = (id, todo) => {
   `;
   const queryParams = [id, todo.task, todo.completed];
 
-  return pool.query(queryString, queryParams).catch((error) => {
+  try {
+    return await pool.query(queryString, queryParams);
+  } catch (error) {
     throw new Error(error);
-  });
+  }
 };
 
-const removeTodo = (id) => {
+/** Batch edit the completion status of multiple todos in the database
+ *
+ * @param {Number[]} ids - An array of IDs for todos to be edited
+ * @param {Object} completed - The completion status to assign to these todos
+ */
+const editTodosCompleted = async (ids, completed) => {
+  const queryString = `
+    UPDATE todos
+    SET completed = $2
+    WHERE id = ANY($1::int[]);
+  `;
+  const queryParams = [ids, completed];
+  console.log('queryParams:', queryParams);
+
+  try {
+    return await pool.query(queryString, queryParams);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+/** Remove an individual todo from the database
+ *
+ * @param {Number} id - The ID of the todo to be removed
+ */
+const removeTodo = async (id) => {
   const queryString = `DELETE FROM todos WHERE id=$1;`;
   const queryParams = [id];
 
-  return pool.query(queryString, queryParams).catch((error) => {
+  try {
+    return await pool.query(queryString, queryParams);
+  } catch (error) {
     throw new Error(error);
-  });
+  }
 };
 
-module.exports = { getTodos, addTodo, editTodo, removeTodo };
+/** Batch remove multiple todos from the database
+ *
+ * @param {Number[]} ids - An array of IDs for todos to be removed
+ */
+const removeTodos = async (ids) => {
+  // Note: `WHERE id IN $1::int[]` does not work for whatever reason, but the
+  // following does:
+  const queryString = `DELETE FROM todos WHERE id = ANY($1::int[]);`;
+  const queryParams = [ids];
+  console.log("queryParams:", queryParams);
+
+  try {
+    return await pool.query(queryString, queryParams);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+module.exports = {
+  getTodos,
+  addTodo,
+  editTodo,
+  editTodosCompleted,
+  removeTodo,
+  removeTodos,
+};
--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -3,6 +3,9 @@ const todoRouter = express.Router();
 const todoQuery = require("../queries/todo.query");
 
 // GET
+/**
+ * @api {get} /todo - Get the list of todo items
+ */
 todoRouter.get("/", (_, res) => {
   todoQuery
     .getTodos()
@@ -14,6 +17,9 @@ todoRouter.get("/", (_, res) => {
 });
 
 // POST
+/**
+ * @api {post} /todo - Add an individual todo item
+ */
 todoRouter.post("/", (req, res) => {
   const newTodo = req.body;
 
@@ -27,12 +33,41 @@ todoRouter.post("/", (req, res) => {
 });
 
 // PUT
-todoRouter.put("/:todoId", (req, res) => {
-  const todoId = req.params.todoId;
+/**
+ * @api {delete} /todo/:id - Edit an individual todo item
+ * @apiParam {Number} id - The ID of the todo item to be edited
+ */
+todoRouter.put("/:id", (req, res) => {
+  const id = req.params.id;
   const editedTodo = req.body;
 
   todoQuery
-    .editTodo(todoId, editedTodo)
+    .editTodo(id, editedTodo)
+    .then(() => res.sendStatus(204))
+    .catch((error) => {
+      console.error(error);
+      res.sendStatus(500);
+    });
+});
+
+const parseCsvNumbers = (csvNumbers) => {
+  return csvNumbers.split(",").map(Number);
+};
+
+/**
+ * @api {put} /todo - Batch edit multiple todo items, modifying only the
+ * completion status
+ * @apiQuery {String} ids - Comma separated list of IDs to be edited
+ * @apiQuery {String="true","false"} completed - The value to assign to the compl
+ */
+todoRouter.put("/", (req, res) => {
+  const ids = parseCsvNumbers(req.query.ids);
+  const completed = req.query.completed.toLowerCase() === 'true';
+  console.log("Editing ids:", ids);
+  console.log("completed:", completed);
+
+  todoQuery
+    .editTodosCompleted(ids, completed)
     .then(() => res.sendStatus(204))
     .catch((error) => {
       console.error(error);
@@ -41,12 +76,33 @@ todoRouter.put("/:todoId", (req, res) => {
 });
 
 // DELETE
-todoRouter.delete("/:todoId", (req, res) => {
-  const todoId = req.params.todoId;
+/**
+ * @api {delete} /todo/:id - Delete an individual todo item
+ * @apiParam {Number} id - The ID of the todo item to be deleted
+ */
+todoRouter.delete("/:id", (req, res) => {
+  const id = req.params.id;
 
-  console.log("Received DELETE request for", todoId);
+  console.log("Received DELETE request for", id);
   todoQuery
-    .removeTodo(todoId)
+    .removeTodo(id)
+    .then(() => res.sendStatus(204))
+    .catch((error) => {
+      console.error(error);
+      res.sendStatus(500);
+    });
+});
+
+/**
+ * @api {delete} /todo - Batch remove multiple todo items
+ * @apiQuery {String} ids - Comma separated list of IDs to be removed
+ */
+todoRouter.delete("/", (req, res) => {
+  const ids = parseCsvNumbers(req.query.ids);
+  console.log("ids:", ids);
+
+  todoQuery
+    .removeTodos(ids)
     .then(() => res.sendStatus(204))
     .catch((error) => {
       console.error(error);
