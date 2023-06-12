7e89a85ba7e1d2619459a348392f84532266ff05
Andreas Copan <avcopan@gmail.com>
2023-06-12 15:05:12 -0400

    Implement to submit textarea on enter key down

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -6,23 +6,31 @@ export function TaskForm({ refreshState }) {
   // This will store the input for adding a new task
   const [taskInput, setTaskInput] = useState("");
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    addTodo(taskInput, refreshState);
+    setTaskInput("");
+  };
+
+  const submitOnEnter = (event) => {
+    if (event.key === "Enter" && !event.shiftKey) {
+      handleSubmit(event);
+    }
+  };
+
   return (
-    <form
-      onSubmit={(event) => {
-        event.preventDefault();
-        addTodo(taskInput, refreshState);
-        setTaskInput("");
-      }}
-      className="taskForm"
-    >
+    <form onSubmit={handleSubmit} className="taskForm">
       <textarea
         placeholder="Describe the next task"
         value={taskInput}
         onChange={(event) => {
           setTaskInput(event.target.value);
         }}
+        onKeyDown={submitOnEnter}
       />
-      <button type="submit" title="add task">+</button>
+      <button type="submit" title="add task">
+        +
+      </button>
     </form>
   );
 }
